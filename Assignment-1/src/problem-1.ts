51d8c9398d6fddaf3a19167e8e865b5525fb87f5
devJayed <jayedhossain.dev@gmail.com>
2024-11-09 18:56:37 +0600

    First Problem-1 update commit

--- Assignment-1/src/problem-1.ts
+++ Assignment-1/src/problem-1.ts
@@ -1,1 +1,9 @@
-console.log("Testing Problem 1");
+{
+  //
+  function sumArr(numbers: number[]): number {
+    return numbers.reduce((acc, curr) => acc + curr, 0);
+  }
+  const result: number = sumArr([1, 2, 3, 4, 5]);
+  console.log(result);
+  //
+}
